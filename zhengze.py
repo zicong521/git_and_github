1673532c1c07e844fabb67a3e819bf444d5b9463
zicong <823891202@qq.com>
2019-10-03 23:36:50 +0800

    today's work

--- zhengze.py
+++ zhengze.py
@@ -6,19 +6,33 @@ Created on Thu Oct  3 22:31:40 2019
 @author: 子聪
 """
 
+def fun1(s,p):
+    pass
+def fun2(s,p):
+    pass
+def fun3(s,p):
+    pass
+def fun4(s,p):
+    pass
 def is_match(s,p):
     #考虑空值
-    if s==NULL and p==NULL:
-        retrun True
-    elif s==NULL or p==NULL:
-        retrun False
+    if s=='' and p=='':
+        return True
+    elif s=='' or p=='':
+        return False
     #四种情况
-
-
+    if p.find('*') == -1 and p.find('.') == -1:
+        return fun1(s,p)
+    if p.find('*') == -1 and p.find('.') != -1:
+        return fun2(s,p)
+    if p.find('*') != -1 and p.find('.') == -1:
+        return fun3(s,p)
+    if p.find('*') != -1 and p.find('.') != -1:
+        return fun4(s,p)
 
 if __name__ == "__main__":
-    s=input('please input the original string = ')
-    p=input('please input the Match Rule = ')
+    s=str(input('please input the original string = '))
+    p=str(input('please input the Match Rule = '))
     if(is_match(s,p)):
         print(True)
     else:
